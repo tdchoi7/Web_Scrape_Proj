965cff4439e08a48d7dee7d4f26c317605f3f20a
tdchoi123 <tdchoi7@gmail.com>
2020-12-08 18:10:55 -0800

    Update pipelines.py
    Finished Product

--- tripadv/tripadv/pipelines.py
+++ tripadv/tripadv/pipelines.py
@@ -4,15 +4,6 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
-
-# class TripadvPipeline:
-#     def process_item(self, item, spider):
-#         return item
-
-
 from scrapy.exporters import CsvItemExporter
 
 # MUST CHANGE TO THIS CLASS IN SETTINGS ITEM_PIPELINES
@@ -34,53 +25,3 @@ class WriteItemPipeline(object):
         self.exporter.export_item(item)
         return item
 
-
-# class PerYearXmlExportPipeline:
-#     """Distribute items across multiple XML files according to their 'year' field"""
-
-#     def open_spider(self, spider):
-#         self.year_to_exporter = {}
-
-#     def close_spider(self, spider):
-#         for exporter in self.year_to_exporter.values():
-#             exporter.finish_exporting()
-
-#     def _exporter_for_item(self, item):
-#         adapter = ItemAdapter(item)
-#         year = adapter['year']
-#         if year not in self.year_to_exporter:
-#             f = open(f'{year}.xml', 'wb')
-#             exporter = XmlItemExporter(f)
-#             exporter.start_exporting()
-#             self.year_to_exporter[year] = exporter
-#         return self.year_to_exporter[year]
-
-#     def process_item(self, item, spider):
-#         exporter = self._exporter_for_item(item)
-#         exporter.export_item(item)
-#         return item
-
-# # exports items from each start_url to a separate CSV
-# class PerUrlCsvExportPipeline:
-
-#     def open_spider(self, spider):
-#         self.url_to_exporter = {}
-
-#     def close_spider(self, spider):
-#         for exporter in self.url_to_exporter.values():
-#             exporter.finish_exporting()
-
-#     def _exporter_for_item(self, item):
-#         adapter = ItemAdapter(item)
-#         url = adapter['url']
-#         if url not in self.url_to_exporter:
-#             f = open('{}.csv'.format(url), 'wb')
-#             exporter = CsvItemExporter(f)
-#             exporter.start_exporting()
-#             self.url_to_exporter[url] = exporter
-#         return self.url_to_exporter[url]
-
-#     def process_item(self, item, spider):
-#         exporter = self._exporter_for_item(item)
-#         exporter.export_item(item)
-#         return item
